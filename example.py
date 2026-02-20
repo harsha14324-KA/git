f9ebbac3435e485265611b351fcfc94480e9aa01
Harsha Vardhan <harshavardhan4762@gmail.com>
2026-02-20 22:27:10 +0530

    py2changes (#3)
    Co-authored-by: tejaswini <tejaswinirajappa5@gmail.com>

--- example.py
+++ example.py
@@ -3,3 +3,10 @@ for i in range(num):
     for j in range(i,num):
         print("*",end="")
     print()
+
+
+num=int(input("enter a number:"))
+for i in range(num+1):
+    for j in range(i):
+        print(i,end="")
+    print()
